9e0e25c7967417a79140eb514443eeff4364f804
Agus <aagiribar@yahoo.es>
2023-10-01 13:52:03 +0100

    Attributes added to Person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,27 @@
+import java.time.LocalDate;
+
 public class Person {
+    private final String name;
+    private final LocalDate birthdate;
+
+    public Person(String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public int getAge() {
+        return toYear(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+    }
+
+    private int toYear(long days) {
+        return (int) (days / 365.25);
+    }
 }
